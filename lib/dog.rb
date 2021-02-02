532b70c388eb217e119cecf0e3ceb9988c2868bf
nicholasdegrate <nicholasdegrate@gmail.com>
2021-02-01 21:01:56 -0600

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,8 @@
 class Dog
+
+    =begin 
+        # Defines a named attribute for this module, where the name is symbol.id2name, creating an instance variable (@name) and a corresponding access method to read it. Also creates a method called name= to set the attribute. String arguments are converted to symbols.
+    =end
     attr_accessor :name, :breed
   
     def initialize(params = {})
@@ -10,3 +14,4 @@ class Dog
       "#{color} #{type}"
     end
 end
+å
